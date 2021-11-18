b6997007dbca4cd83b2e890068006accb9a72640
TANYA <92921547+MMNLDR@users.noreply.github.com>
2021-11-18 21:02:54 +0800

    Add files via upload

---
+++ 其他/模拟实现strlen.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int my_strlen(char* str)
+{
+	char* start = str;
+	char* end = str;
+	while(*end != '\0')
+	{
+		end++;
+	}
+	return end - start;
+} 
+
+int main()
+{ 
+	//strlen - 求字符串长度
+	//递归 - 模拟实现strlen - 计数器的方式1，递归的方式2 
+	
+	char arr[] = "bit";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+
+	return 0;
+}
